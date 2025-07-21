6b5a8eeab657e857c6e35976506c7ce46d9e8db1
5ceansdeep <su5653@naver.com>
2025-07-21 21:59:12 +0900

    런타임 에러 코드

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import Load from "./load.js";
-import Main from "./main.js";
 import Login from "./login.js";
-import sideMenu from "./sideMenu.js";
+import Sidemenu from "./sideMenu.js";
 
 function App() {
   return (
     <div>
       {/* <Load />
       <Main /> */}
-      <Login />
-      <h1>dks</h1>
-      {/* <sideMenu /> */}
+      {/* <Login /> */}
+      <Sidemenu />
     </div>
   );
 }
