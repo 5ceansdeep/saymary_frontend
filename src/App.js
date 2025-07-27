9955073c22d5f42e7d834c8fc91cde060d968548
5ceansdeep <su5653@naver.com>
2025-07-27 23:57:28 +0900

    비밀번호 재설정 / 이메일 입력, 비밀번호 재설정 / 새로운 비밀번호 입력 페이지 구현

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import Load from "./load.js";
 import Main from "./main.js";
 import Login from "./login.js";
 import Register from "./register.js";
+import Forgot from "./forgot.js";
+import Newpw from "./newpw.js";
 
 function App() {
   return (
     <div>
       {/* <Load />
       <Main /> */}
-      <Login />
+      {/* <Login /> */}
       {/* <Register /> */}
+      {/* <Forgot /> */}
+      <Newpw />
     </div>
   );
 }
