a448cee55ab869ddaffd17d868807c246ee2085f
5ceansdeep <su5653@naver.com>
2025-08-07 01:21:46 +0900

    http

--- src/login/login.js
+++ src/login/login.js
@@ -53,7 +53,7 @@ function Login() {
     setError(null);
 
     try {
-      const response = await fetch("http://3.34.19.178:8080/api/user/login", {
+      const response = await fetch("https://3.34.19.178:8080/api/user/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
